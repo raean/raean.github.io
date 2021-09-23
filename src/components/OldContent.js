51fc8e8f129d3166feb2dda82a8d81de1c179323
Rae Abunahla <raghebabn@gmail.com>
2021-09-23 10:11:13 -0400

    gh-pages fix

--- src/components/OldContent.js
+++ src/components/OldContent.js
@@ -45,7 +45,7 @@ export class OldContent extends React.Component {
                     </a>
                     </div>
                     <ul class="menu">
-                    <li><a href="#home">Home</a></li>
+                    <li><a href="/">Home</a></li>
                     <li><a href="#about">About</a></li>
                     <li><a href="/design-projects"> Design </a></li>
                     <li><a href="/code-projects"> Code </a></li>
